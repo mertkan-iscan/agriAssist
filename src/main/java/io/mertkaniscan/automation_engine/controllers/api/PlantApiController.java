3b9fc7c3c05b6ca93e44e567143743e5cca72943
mertkan-iscan <mertiscan34@gmail.com>
2024-11-30 05:25:59 +0300

    Ignore myenv folder

--- src/main/java/io/mertkaniscan/automation_engine/controllers/api/PlantApiController.java
+++
@@ -1,9 +0,0 @@
-package io.mertkaniscan.automation_engine.controllers.api;
-
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/plants")
-public class PlantApiController {
-
-}
--- src/main/java/io/mertkaniscan/automation_engine/repositories/FieldRepository.java
+++
@@ -1,9 +0,0 @@
-package io.mertkaniscan.automation_engine.repositories;
-
-import io.mertkaniscan.automation_engine.models.Field;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FieldRepository extends JpaRepository<Field, Integer> {
-}
